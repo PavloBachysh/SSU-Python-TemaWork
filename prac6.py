681b5307df034698780ae6691d15bbb82d5530d2
PavloBachysh <happytouni@gmail.com>
2025-10-29 13:22:40 +0100

    Start
    Я, тімлід, сторив репозиторій, доєднав інших учасників. Також я створив інтерфейс взаємодії з програмою, а аткож першу функцію - додавання в список словників-студнетів

---
+++ prac6.py
@@ -0,0 +1,100 @@
+#Додавання студенту в список - Бачиш Павло  DONE
+#Видалення студента з списку - Борзаниця Олександр
+#Сортування - Грінченко Дмитро
+#Вивід - Сагайдак Богдан
+
+#Список з усіма студентами
+allStudents = []
+
+
+#Функція додавання нового студента в список
+def AddStudent():
+    try:
+        group = str(input("Введіть групу студента: "))
+        pib = str(input("Введіть ПІБ студента: "))
+        course = int(input("Введіть номер курсу студента (числом): "))
+    except:
+        print("Некоректно введені дані")
+        return
+
+    sub = []
+    mark = []
+
+    while(True):
+        s = str(input("Введіть назву предмету (Щоб закінчити ввод предметів і оцінок - введіть STOP): "))
+        if (s == "STOP"):
+            break
+        while (True):
+            try:
+                m = int(input(f"Введіть оцінку предмета {s} (числом): "))
+                break
+            except:
+                print("Некоректно введені дані")
+                continue
+
+        sub.append(s)
+        mark.append(m)
+
+    subjects = dict(zip(sub, mark))
+
+    student = {
+    "Group": group,
+    "PIB": pib,
+    "Course": course,
+    "Subjects": subjects
+    }
+
+
+    #Чи є такий студент вже в списку
+    for st in allStudents:
+        if(st.get("PIB") == pib):
+            print(
+                "Такий студент вже є у списку\n",
+                "1 - Це інший студент, внести його в список",
+                "\n2 - Змінити дані студента"
+                )
+            try:
+                funcNum = int(input("Оберіть одну з запропонованих функцій: "))
+            except:
+                print("Некоректно введені дані")
+                return
+            match funcNum:
+                case 1:
+                    allStudents.append(student)
+                    return
+                case 2:
+                    allStudents[allStudents.index(st)] = student
+                    return
+
+    allStudents.append(student)
+
+
+
+
+#Сам функціонал програми
+while(True):
+    print(
+        "1 - Додавання студента",
+        "\n2 - Видалення студента",
+        "\n3 - Сортування",
+        "\n4 - Вивести список стдентів"
+        "\n5 - Завершити програму",
+    )
+    try:
+        funcNum = int(input("Оберіть одну з запропонованих функцій: "))
+    except:
+        print("Некоректно введені дані")
+        break
+    match funcNum:
+        case 1:
+            AddStudent()
+        case 2:
+            print("Видалення")
+        case 3:
+            print("Сортування")
+        case 4:
+            print("Вивід")
+        case 5:
+            break
+
+
